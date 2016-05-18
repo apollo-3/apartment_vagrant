f6fd4480389cee08d1d74af492290e57ec33f4af
U-MINSK\Andrei_Semionau <Andrei_Semionau@EPBYMINW5697.minsk.epam.com>
2016-05-18 15:34:33 +0300

    fixed cookbooks

--- cookbooks/npm/recipes/to_prod_patch.rb
+++ cookbooks/npm/recipes/to_prod_patch.rb
@@ -1,7 +1,7 @@
 bash "change_ip_in_sources" do
   code <<-EOH
-    sed -i 's/192\.168\.33\.123/estate-hunt\.com/g' "#{node[:ui][:home]}/public/providers/values.js"
-    sed -i 's/192\.168\.33\.123/estate-hunt\.com/g' "#{node[:api][:home]}/classes/helper.rb"    
+    sed -i 's/192\.168\.33\.123/estate-hunt\.com/g' node[:ui][:home]/public/providers/values.js
+    sed -i 's/192\.168\.33\.123/estate-hunt\.com/g' {node[:api][:home]/classes/helper.rb
   EOH
   only_if { ::File.exists? "#{node[:ui][:home]}/public/providers/values.js" && ::File.exists? "#{node[:api][:home]}/classes/helper.rb"}
 end
